055efd8dade760718aaa901033e3e636e1c2f72c
dzlatarov <danzlatarov1@gmail.com>
2020-11-08 18:18:02 +0200

    initial commit

---
+++ Fitness-App.Web/Fitness-App.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Fitness_App.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
